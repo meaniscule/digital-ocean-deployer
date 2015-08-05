92010947ac6012cdd86e909ce1544f8ec9b9d747
Ash Ryan <ash@ashryanbeats.com>
2015-08-05 13:50:30 -0400

    Nginx update and reload.

--- input.sh
+++ input.sh
@@ -8,17 +8,24 @@ echo Please enter your port
 read PORT
 echo "Ok, port $PORT on $SUBDOMAIN for user $USERNAME."
 
-cat >> sample << EOM
+cd ~/../../etc/nginx/sites-available/
+
+sudo cat >> default << EOM
 
 server {
   listen 80;
   server_name $SUBDOMAIN;
   location / {
-    proxy_set_header   X-Real-IP $remote_addr;
-    proxy_set_header   Host      $http_host;
+    proxy_set_header   X-Real-IP \$remote_addr;
+    proxy_set_header   Host      \$http_host;
     proxy_pass         http://127.0.0.1:$PORT;
   }
 }
 EOM
 
-echo "$(<sample)"
+echo "Your NGINX server was added:"
+echo "$(<default)"
+
+nginx -s reload
+echo "NGINX has been reloaded."
+
